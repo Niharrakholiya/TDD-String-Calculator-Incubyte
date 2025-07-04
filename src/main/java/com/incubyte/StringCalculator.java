89a3e8cdef99013318da7e67dc19919166401d72
Niharrakholiya <nihar.rakholiya526@gmail.com>
2025-07-05 00:29:39 +0530

    [Green Step-5] implemented support to throw exception on negative numbers

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -1,19 +1,28 @@
 package com.incubyte;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
     public static int add(String input) {
         if (input == null || input.isEmpty()) return 0;
 
+        int sum =0;
         String[] tokens = input.split(",|\\n");
-        int sum = 0;
+        List<Integer> negatives = new ArrayList<>();
 
-        for (String token : tokens) {
-            if (!token.isEmpty()) {
-                sum += parseInt(token);
+        for (String t : tokens) {
+            int n = Integer.parseInt(t);
+            if (n < 0) {
+                negatives.add(n);
             }
+            sum += n;
+        }
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("negatives not allowed: " + negatives.get(0));
         }
         return sum;
     }
