4391ffec55c8ccf8a9212876e5a22cac98cf4127
Niharrakholiya <nihar.rakholiya526@gmail.com>
2025-07-04 23:17:28 +0530

    [Refactor Step-4] removed redundant condition and simplified token parsing for all inputs

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -4,21 +4,17 @@ package com.incubyte;
 import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
-        public static int add(String input) {
-            if(input == null || input.isEmpty()) return 0;
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) return 0;
 
-            if (!input.contains(",") && !input.contains("\n")) {
-                return Integer.parseInt(input);
+        String[] tokens = input.split(",|\\n");
+        int sum = 0;
+
+        for (String token : tokens) {
+            if (!token.isEmpty()) {
+                sum += parseInt(token);
             }
-            String[] tokens = input.split(",|\\n");
-
-            int sum = 0;
-            for (String t : tokens) {
-                if (!t.isEmpty()) {
-                    sum += Integer.parseInt(t);
-                }
-            }
-            return sum;
-
         }
+        return sum;
+    }
 }
