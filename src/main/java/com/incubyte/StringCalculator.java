a3c86c8b51c97533127574bdfd8838ba11273a34
Niharrakholiya <nihar.rakholiya526@gmail.com>
2025-07-04 21:04:36 +0530

    [Green Step-2] single-number input returns number

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -1,8 +1,12 @@
 package com.incubyte;
 
 
+import static java.lang.Integer.parseInt;
+
 public class StringCalculator {
         public static int add(String input) {
-            return 0;
+            if(input == null || input.isEmpty()) return 0;
+
+            return parseInt(input);
         }
 }
