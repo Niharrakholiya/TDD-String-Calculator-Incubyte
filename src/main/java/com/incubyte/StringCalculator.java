04b8a1ed0e220a40a8438de231efe383a79975c5
Niharrakholiya <nihar.rakholiya526@gmail.com>
2025-07-04 23:15:42 +0530

    [Green Step-4] implemented support for both comma and newline delimiters in input string

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -10,7 +10,7 @@ public class StringCalculator {
             if (!input.contains(",") && !input.contains("\n")) {
                 return Integer.parseInt(input);
             }
-            String[] tokens = input.split(",");
+            String[] tokens = input.split(",|\\n");
 
             int sum = 0;
             for (String t : tokens) {
