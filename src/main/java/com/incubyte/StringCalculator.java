687fb66f804061e2c45f68dfdaf7782cd332fb2d
Niharrakholiya <nihar.rakholiya526@gmail.com>
2025-07-05 08:49:44 +0530

    [Refactor Step-7] extracted getDelimiter() method to encapsulate custom delimiter parsing logic

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -2,6 +2,7 @@ package com.incubyte;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
 
@@ -16,10 +17,25 @@ public class StringCalculator {
         }
     }
 
+    private static String getDelimiter(String input) {
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+            String rawDelimiter = input.substring(2, delimiterIndex);
+            return Pattern.quote(rawDelimiter);
+        }
+        return ",|\n";
+    }
+
+
     public static int add(String input) {
         if (input == null || input.isEmpty()) return 0;
 
-        String[] tokens = input.split(",|\\n");
+        String delimiter = getDelimiter(input);
+        if (input.startsWith("//")) {
+            input = input.substring(input.indexOf("\n") + 1);
+        }
+
+        String[] tokens = input.split(delimiter);
         List<Integer> negatives = new ArrayList<>();
         int sum = 0;
 
