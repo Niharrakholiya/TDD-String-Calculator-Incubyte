04ae3a85a2d065255c3b63b545052e12757646ee
Niharrakholiya <nihar.rakholiya526@gmail.com>
2025-07-05 08:38:48 +0530

    [Green Step-6] updated implementation to throw exception listing all negative numbers found in input

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import static java.lang.Integer.parseInt;
 
+
 public class StringCalculator {
+
     public static int add(String input) {
         if (input == null || input.isEmpty()) return 0;
 
